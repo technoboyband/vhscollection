85fa1db05b0119cac1cc501419cf762ec77c813f
technoboyband <jabyyp@gmail.com>
2026-05-01 12:12:51 -0700

    working hover tween in vhs1 gdscript

--- vhs1_mouse.gd
+++ vhs1_mouse.gd
@@ -1,12 +1,30 @@
 extends Node2D
 
+var initialY: float
+var tween: Tween
+var isRaised = false
 
+func _ready() -> void:
+	initialY = $vhs1.position.y
 
 func _on_area_2d_mouse_entered() -> void:
-	translate(Vector2(0, -10))  # same result
+	if isRaised:
+		return
+	if tween:
+		tween.kill()
+	tween = get_tree().create_tween() 
+	tween.tween_property($vhs1, "position:y", initialY - 10, .2)
+	isRaised = true
 	pass # Replace with function body.
 
 
 func _on_area_2d_mouse_exited() -> void:
-	translate(Vector2(0, 10))  # same result
-	pass # Replace with function body.
+	if !isRaised:
+		return 
+	if tween:
+		tween.kill()
+	tween = get_tree().create_tween()
+	
+	tween.tween_property($vhs1, "position:y", initialY, .2)
+	isRaised = false
+	pass # Replace with function body. Replace with function body.
